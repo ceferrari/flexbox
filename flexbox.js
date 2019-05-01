27b83fc9f567ee7bdd92905d89a715ae4e7ee1ed
Carlos Eduardo Ferrari <ceferrari@gmail.com>
2019-05-01 20:48:13 -0300

    improvements

--- flexbox.js
+++ flexbox.js
@@ -1,19 +1,29 @@
-$(function () {
-	"use strict"
-	$(window).bind("resize", function () {
-		const w = $(this).width();
-		$("#app").removeClass("toggled", w == 768 || w == 1200);
-	});
-	$(".sidebar-toggle").click(function () {
-		$("#app").toggleClass("toggled");
-	});
-	$("#sidebar-overlay").click(function () {
-		$("#app").removeClass("toggled");
-	});
-	$("#app-sidebar>.nav-sidebar").niceScroll({
-		horizrailenabled: false
-	});
-	new ResizeSensor(document.getElementById("app-sidebar"), function() {
-		$("#app-sidebar>.nav-sidebar").getNiceScroll().resize();
-	});
-})
+$(function() {
+  "use strict";
+
+  $(window).bind("resize", function() {
+    const w = $(this).width();
+    $("#app").removeClass("toggled", w == 768 || w == 1200);
+  });
+
+  $(".back-to-top").click(function() {
+    $("#app-content").animate({ scrollTop: 0 }, "slow");
+    return false;
+  });
+
+  $(".sidebar-toggle").click(function() {
+    $("#app").toggleClass("toggled");
+  });
+
+  $("#sidebar-overlay").click(function() {
+    $("#app").removeClass("toggled");
+  });
+
+  $("#app-sidebar>.nav-sidebar").niceScroll({ horizrailenabled: false });
+
+  new ResizeSensor(document.getElementById("app-sidebar"), function() {
+    $("#app-sidebar>.nav-sidebar")
+      .getNiceScroll()
+      .resize();
+  });
+});
